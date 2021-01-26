3a3fd9f3cb700e853d3f038ff8f143beee71c4ad
ManBytesDog <ThomasJ.Marstiller@yahoo.com>
2021-01-25 22:10:35 -0500

    made show action for books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,9 @@
 class BooksController < ApplicationController
     def show 
-        book = Book.find_by(:id params[:id])
-        
+        book = Book.find(params[:id])
+        render json: book, except:[:created_at, :updated_at]
     end
+
+    def 
+
 end
